6082f4d01de38c119f69701141880bac8571e189
Ally-3 <146335458+Ally-3@users.noreply.github.com>
2023-12-08 10:36:44 +0000

    changes made to 'email' to accept username, phone number instead of only email

--- src/models/user.js
+++ src/models/user.js
@@ -6,10 +6,10 @@ const User = SQLconnection.define("User",{
         type: DataTypes.STRING,
         unique: true,
         allowNull: false,
-        isEmail: true,
-        validate: {
-            is: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        }
+        // isEmail: true,
+        // validate: {
+        //     is: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        // }
     },
     password: {
         type: DataTypes.STRING,
